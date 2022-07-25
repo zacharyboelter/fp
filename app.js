a020e7148c8a865c81447b428701772bec700145
Zachary Boelter <zboelter@gmail.com>
2022-07-25 11:51:33 -0700

    recursive function for reverse counting instead of loops

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 // functional programming
 
-let currencyOne = 100
-let currencyTwo = 0
-let exchangeRate = 1.2
+// let currencyOne = 100
+// let currencyTwo = 0
+// let exchangeRate = 1.2
 
-function convertCurrency(amount, rate){
-    return amount * rate
+// function convertCurrency(amount, rate){
+//     return amount * rate
+// }
+
+// currencyTwo = convertCurrency(currencyOne, exchangeRate)
+
+// console.log(currencyTwo)
+
+
+// RECURSION
+
+
+let counter = 3
+function example() {
+    console.log(counter)
+    counter -= 1
+    if (counter === 0){
+        return
+    }
+    example()
 }
 
-currencyTwo = convertCurrency(currencyOne, exchangeRate)
-
-console.log(currencyTwo)
+example()
