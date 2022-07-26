60976eb0514d00461958165426e002be03b3fc1e
Zachary Boelter <zboelter@gmail.com>
2022-07-26 14:45:25 -0700

    fp example vs oop

--- app.js
+++ app.js
@@ -13,17 +13,28 @@
 // console.log(currencyTwo)
 
 
-// RECURSION
+var shoes = 100
+var stateTax = 1.2
 
-
-let counter = 3
-function example() {
-    console.log(counter)
-    counter -= 1
-    if (counter === 0){
-        return
-    }
-    example()
+function totalPrice(shoes, tax){
+    return shoes * tax
 }
 
-example()
+var toPay = totalPrice(shoes, stateTax)
+console.log(toPay)
+
+// // RECURSION
+
+
+// let counter = 3
+// function example() {
+//     console.log(counter)
+//     counter -= 1
+//     if (counter === 0){
+//         return
+//     }
+//     example()
+// }
+
+// example()
+
