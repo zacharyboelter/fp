0ccaae9bc5bf9b613a9df8f9793c7db157dacc68
Zachary Boelter <zboelter@gmail.com>
2022-07-28 14:51:45 -0700

    functional programming

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// functional programming
+functional programming
 
-// let currencyOne = 100
-// let currencyTwo = 0
-// let exchangeRate = 1.2
+let currencyOne = 100
+let currencyTwo = 0
+let exchangeRate = 1.2
 
-// function convertCurrency(amount, rate){
-//     return amount * rate
-// }
+function convertCurrency(amount, rate){
+    return amount * rate
+}
 
-// currencyTwo = convertCurrency(currencyOne, exchangeRate)
+currencyTwo = convertCurrency(currencyOne, exchangeRate)
 
-// console.log(currencyTwo)
+console.log(currencyTwo)
 
 
 var shoes = 100
